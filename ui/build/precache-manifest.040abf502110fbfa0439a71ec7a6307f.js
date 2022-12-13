616b878c774c049b07688e4ce79d4690be2cdf6f
Vincent Duprez <vincent@site47.fr>
2022-12-13 20:25:36 +0100

    2blox

---
+++ ui/build/precache-manifest.040abf502110fbfa0439a71ec7a6307f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e583faf1bfc739f93a2aa67d864da31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f18b154c110088777c6d",
+    "url": "/static/css/2.c38cd9da.chunk.css"
+  },
+  {
+    "revision": "f18b154c110088777c6d",
+    "url": "/static/js/2.50505ba2.chunk.js"
+  },
+  {
+    "revision": "28ce00ad444b69bea42850e06eec9da3",
+    "url": "/static/js/2.50505ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e3a5b88f514bea8e023",
+    "url": "/static/js/main.3b068b16.chunk.js"
+  },
+  {
+    "revision": "0e903404fe668039b1a7",
+    "url": "/static/js/runtime-main.2a78626f.js"
+  }
+]);
